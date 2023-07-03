9498a7321c55497a92d474e9b68b5eba185b2665
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-03 18:35:04 +0800

    If data not found from api

--- src/Pages/BlogDetail.js
+++ src/Pages/BlogDetail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 export default function BlogDetail() {
   const [article, setArticle] = useState({});
   const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
   const params = useParams();
 
   useEffect(
@@ -11,6 +12,10 @@ export default function BlogDetail() {
         const request = await fetch(
           "https://api.spaceflightnewsapi.net/v3/articles/" + params.id
         );
+
+        if (!request.ok) {
+          return setNotFound(true);
+        }
 
         const response = await request.json();
 
@@ -22,6 +27,10 @@ export default function BlogDetail() {
     [params]
   );
 
+  if(notFound){
+    return <h1>Maaf artikel tidak ditemukan!</h1>
+  }
+
   return (
     <div>
       {loading ? (
@@ -30,7 +39,9 @@ export default function BlogDetail() {
           <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
           <img src={article.imageUrl} alt={article.title} />
           <p>{article.summary}</p>
-          <div>Source : <a href={article.url}>{article.newsSite}</a></div>
+          <div>
+            Source : <a href={article.url}>{article.newsSite}</a>
+          </div>
         </article>
       ) : (
         <i>Loading Data ...</i>
