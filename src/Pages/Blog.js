dfcbf985c5d964841f78886a57f7561db2a3dc4e
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-03 14:47:07 +0800

    Blog API

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,3 +1,37 @@
+import { useState, useEffect } from "react";
+
 export default function Blog() {
-  return <h1>Ini adalah Blog</h1>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function () {
+    async function getArticle() {
+      const request = await fetch(
+        "https://api.spaceflightnewsapi.net/v3/articles"
+      );
+
+      const response = await request.json();
+
+      setArticles(response);
+      setLoading(true);
+    }
+    getArticle();
+  }, []);
+
+  return (
+    <>
+      <h1>Halaman Blog</h1>
+      <p>Ini adalah article saya</p>
+      {loading ?
+        <article>
+            {articles.map(function (article) {
+            return (
+                <li>Title : {article.title}</li>
+            );
+            })}
+        </article>
+    
+      : <i>Loading Articles ...</i>}
+    </>
+  );
 }
